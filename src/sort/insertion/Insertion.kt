94b976770724f70cf1277bd4883c7c83f79b9ec1
eugenekorolev <100HzJugin@gmail.com>
2017-08-26 01:09:05 +0300

    Insertion sort was implimanted Output was corrected

--- src/sort/insertion/Insertion.kt
+++ src/sort/insertion/Insertion.kt
@@ -17,14 +17,28 @@ import java.util.*
  */
 class Insertion : Sort() {
     override fun sort(array: Array<Int>): Array<Int> {
-        return array
+        var element : Int
+        val tmp :  Array<Int> = array
+        val length : Int = array.size - 1
+        for (i in 1..length){
+            element = array[i]
+            var j : Int = i - 1
+            while(j >= 0 && array[j] > element){
+                array[j + 1] = array[j]
+                j = j.dec()
+            }
+            array[j + 1] = element
+        }
+        return tmp
     }
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             val insertion = Insertion()
             val array = Array<Int>(Random().nextInt(30), { i -> Random().nextInt(30)})
-            insertion.sort(array).forEach(System.out::println)
+            array.forEach{print(it.toString() + " ")}
+            println()
+            insertion.sort(array).forEach{print(it.toString() + " ")}
         }
     }
 }
