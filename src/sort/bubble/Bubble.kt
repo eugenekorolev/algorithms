6cf79288801af49cdeb93bfb5a329ed394741619
eugenekorolev <100HzJugin@gmail.com>
2017-08-25 20:54:48 +0300

    1) bubble sort was impleneted 2) swap method was added to interface

--- src/sort/bubble/Bubble.kt
+++ src/sort/bubble/Bubble.kt
@@ -1,41 +1,43 @@
 package sort.bubble
 
+import java.util.*
 
 
 /**
- * Created by машинко on 24.08.2017.
+ * Bubble sort by Kvendyz
  */
 class Bubble:Sort{
-    override fun sort(array: List<Int>): List<Int> {
-        val arrayLength : Int = array.size;
-        var hm = array.toTypedArray();
+
+    override fun swap(array: Array<Int>, startPos: Int, nextPos: Int): Array<Int> {
+        val tmp = array[nextPos]
+        array[nextPos] = array[startPos]
+        array[startPos] = tmp
+        return array
+    }
+
+    override fun sort(array: Array<Int>): Array<Int> {
+        val arrayLength : Int = array.size
         for (i in 0..(arrayLength - 2)){
-            var F = 0;
-            for (j in 0..(arrayLength - i)){
-                if (hm[i] < hm[i + 1]){
-                    var tmp = hm[i + 1]
-                    hm[i + 1] = hm[i]
-                    hm[i] = tmp
+            var F = 0
+            for (j in 0..(arrayLength - 2 - i)){
+                if (array[j] > array[j + 1]){
+                    swap(array, j, j + 1)
                     F = 1
                 }
                 if (F == 0) break
             }
         }
-        return hm.asList()
+        return array
     }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val b = Bubble();
-            var list = b.sort(b.asList(1, 2, 5, 3, 2, -7, 3, 6))
-            println(list)
+            val b = Bubble()
+            val array = Array<Int>(8, {i -> Random().nextInt(30)})
+            b.sort(array).forEach(System.out::println)
         }
     }
-
-    fun <T> asList(vararg ts: T): List<T> {
-        val result = ArrayList<T>()
-        for (t in ts)
-            result.add(t)
-        return result
-    }
 }
+
+
