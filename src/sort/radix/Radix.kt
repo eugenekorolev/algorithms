d777d18564a17e5b4a70da60918e61e1e14b06e6
eugenekorolev <100HzJugin@gmail.com>
2017-08-31 02:11:14 +0300

    Radix sort was implamented

--- src/sort/radix/Radix.kt
+++ src/sort/radix/Radix.kt
@@ -9,62 +9,56 @@ import java.util.*
 class Radix : Sort() {
     override fun sort(array: Array<Int>): Array<Int> {
 
-        //определить максимальное
-
-
+        val koef = 10
         val it : Iterator<Int> = array.iterator()
-
-        val newArray : Array<Int> = Array(array.size, {i -> 0})
-        var koef = 1
-        //по коллекциии строим гистограмму беря остаток от деления на число кратное 10
-        //сначала десятки, потом сотки, потом тысячи
-
-
-        //найдем максимамальное число в списке
         var max = Int.MIN_VALUE
         while (it.hasNext()){
             val number = it.next()
             if (number > max) max = number
         }
 
-        //пока у нас установлено что все числа в 10ой системе счисления
-
-        var radixNumber = max.toString().length
-//        var count = 0;
-//        do{
-//            radixNumber = max / 10
-//            max /= 10
-//            count = count.inc()
-//        }while (radixNumber != 0)
+        val radixNumber = max.toString().length
         var cloneArray = array.clone()
-        var i = 0
+        val newArray = array.clone()
+        var i : Int
+        i = 0
         while(i < radixNumber) {
-            val hist :  Array<Int> = Array(10, {i -> 0})
-            val func :  Array<Int> = Array(10, {i -> 0})
+            val hist :  Array<Int> = Array(koef, {x -> 0})
+            val func :  Array<Int> = Array(koef, {x -> 0})
             //нужен еще внешний цикл, который будет по разрядам
-            koef *= 10
+
             var j = 0
             while (j < cloneArray.size) {
-                val mod = (cloneArray[j] % koef)
+                val mod = cloneArray[j] % koef
                 hist[mod] = hist[mod].inc()
                 j = j.inc()
             }
             //из гистограммы получаем функцию распределения
             func[0] = hist[0]
-            for (i in 1..(hist.size - 1)) {
-                func[i] = func[i - 1] + hist[i]
-            }
-            for (i in (cloneArray.size - 1) downTo 0) {
-                val number = cloneArray[i] % koef
-                newArray[func[number] - 1] = array[i]
-                func[number] = func[number].dec()
+            for (k in 1..(hist.size - 1)) {
+                func[k] = func[k - 1] + hist[k]
             }
             j = 0
             while (j < cloneArray.size) {
-                cloneArray[j] /= 10
+                cloneArray[j] = newArray[j]
                 j = j.inc()
             }
+
+            for (k in (cloneArray.size - 1) downTo 0) {
+                val q = Math.pow(koef.toDouble(), i.toDouble()).toInt()
+                val r = cloneArray[k] / q
+                val number = r % koef
+                newArray[func[number] - 1] = cloneArray[k]
+                func[number] = func[number].dec()
+            }
             i = i.inc()
+            j = 0
+            cloneArray = newArray.clone()
+            while (j < cloneArray.size) {
+                cloneArray[j] = cloneArray[j] / koef
+                j = j.inc()
+            }
+
 
             println()
             println("Отладочный вывод на шаге" + i)
@@ -82,7 +76,7 @@ class Radix : Sort() {
         fun main(args: Array<String>) {
             val radix = Radix()
             val array = Array<Int>(Random().nextInt(30), { i -> Random().nextInt(30)})
-            println("Исходный массив");
+            println("Исходный массив")
             array.forEach{print(it.toString() + " ")}
             println()
             println()
